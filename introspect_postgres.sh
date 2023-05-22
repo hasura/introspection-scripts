bab5fd974df80f58c68c0ee6f3881397503708ed
Auke Booij <164426+abooij@users.noreply.github.com>
2023-05-22 21:29:13 +0200

    add file overwriting safety check

--- introspect_postgres.sh
+++ introspect_postgres.sh
@@ -4,6 +4,12 @@ set -xeuf -o pipefail
 
 dir="results/postgres"
 
+if [ -d "$dir" ]
+then
+    echo "ERROR: Postgres introspection directory $dir already exists; please rename it"
+    exit 1
+fi
+
 mkdir -p "$dir"
 
 psql "$@" -f pg_table_metadata.sql -o "$dir/tables.json" -A
--- introspect_remote.sh
+++ introspect_remote.sh
@@ -7,9 +7,16 @@ mkdir -p results/remotes
 url="$1"
 
 domain=$(echo "$url" | awk -F/ '{print $3}')
+filename="results/remotes/$domain.json"
+
+if [ -f "$filename" ]
+then
+    echo "ERROR: Remote Schema introspection $filename already exists; please rename it"
+    exit 1
+fi
 
 curl "$@" \
   -X POST \
   -H 'content-type: application/json' \
   --data-binary "@remote-schema.json" \
-  -o "results/remotes/$domain.json"
+  -o "$filename"
